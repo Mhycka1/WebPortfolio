b838745f105ec1d877b6861ea59ea54a89b70e1e
Mhycka1 <mmcdon10@terpmail.umd.edu>
2023-10-30 20:34:12 -0400

    added more javascript features

--- script.js
+++ script.js
@@ -1,38 +1,10 @@
-alert("script.js loaded!")
 
-let varName = 30;
+// Get a reference to the button element
+const clickButton = document.getElementById("click-button");
 
-if(varName > 10){
-    alert("greater")
-}else{
-    alert("less")
-}
-
-let coolList = [
-    3, 5, 7, 9,
-    4, 6, 6, 45,
-]
-
-coolList.push(60);
-
-//hides things by putting it in the console?
-console.log(coolList);
-
-function greetPerson(name){
-    return "Hello " + name;
-}
-
-let titleElement = document.getElementById("title");
-
-function changeTitle(){
-    titleElement.innerText = "wow javascript!";
-    titleElement.style.color = "red";
-}
-
-setTimeout(changeTitle, 1000);
-
-let clickMeBtn = document.getElementById("clickMe")
-
-clickMeBtn.addEventListener("click", changeTitle);
-
+// Add a click event listener to the button
+clickButton.addEventListener("click", function() {
+    // Define the action you want to take when the button is clicked
+    alert("Button clicked! You can add more functionality here.");
+});
 
